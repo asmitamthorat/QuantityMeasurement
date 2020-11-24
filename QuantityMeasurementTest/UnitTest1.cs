01366cfa4d3f886240873c1961efc05faeec7de7
asmitamthorat <asmita26296@gmail.com>
2020-11-24 11:44:33 +0530

    [asmita]ADD:test cases for the feet comparision

--- QuantityMeasurementTest/UnitTest1.cs
+++ QuantityMeasurementTest/UnitTest1.cs
@@ -23,8 +23,33 @@ namespace QuantityMeasurementTest
             QuantityUnits secondUnit = null;
             bool result = quantityMeasurement.EqualityCheck(firstUnit, secondUnit);
             Assert.IsFalse(result);
-
-
         }
+
+        [Test]
+        public void WhenFeetRefference_comparetosameFeetRefference_ShouldReturnTrue() {
+            QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+            QuantityUnits firstUnit = new QuantityUnits((double)UnitConversion.GetQuanityValue(UnitConversion.Units.FEET), 0);
+            bool result = quantityMeasurement.EqualityCheck(firstUnit, firstUnit);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void WhenGivenQuantittyCompareWithFeetType_shouldReturnTrue() {
+            QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+            QuantityUnits firstUnit = new QuantityUnits((double)UnitConversion.GetQuanityValue(UnitConversion.Units.FEET), 0);
+            QuantityUnits SecondUnit = new QuantityUnits((double)UnitConversion.GetQuanityValue(UnitConversion.Units.FEET), 0);
+            bool result = quantityMeasurement.EqualityCheck(firstUnit, SecondUnit);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void WhenfiveFeetComparedWith_twoFeet_ShouldReturnTrue() {
+            QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+            QuantityUnits firstUnit = new QuantityUnits((double)UnitConversion.GetQuanityValue(UnitConversion.Units.FEET),5);
+            QuantityUnits SecondUnit = new QuantityUnits((double)UnitConversion.GetQuanityValue(UnitConversion.Units.FEET),2);
+            bool result = quantityMeasurement.EqualityCheck(firstUnit, SecondUnit);
+            Assert.IsFalse(result);
+        }
+
     }
 }
